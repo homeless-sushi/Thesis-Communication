7799541cf9eada66eb7dda4bf2c7b041170a000e
Guido Rolle <rolleguido@gmail.com>
2023-09-18 12:32:11 +0200

    Fix frequency for board

--- libs/AppRegisterServer/src/Frequency.cc
+++ libs/AppRegisterServer/src/Frequency.cc
@@ -1,5 +1,6 @@
 #include "AppRegisterServer/Frequency.h"
 
+#include <algorithm>
 #include <array>
 #include <iostream>
 #include <memory>
@@ -10,9 +11,9 @@
 
 namespace Frequency
 {
-    FrequencyStep GetCurrentFrequency(unsigned int cpu)
+    CPU_FRQ getCurrCpuFreq()
     {
-        std::string cmd("sudo cpufreq-info -w -c " + std::to_string(cpu));
+        std::string cmd("sudo cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq");
 
         std::array<char, 128> buffer;
         std::string result;
@@ -24,31 +25,162 @@ namespace Frequency
             result += buffer.data();
         }
 
-        return FrequencyStep(std::stoul(result.c_str()));
+        return CPU_FRQ(std::stoul(result.c_str()));
     };
 
-
-    FrequencyStep GetNextFrequency(FrequencyStep freq)
+    CPU_FRQ getNextCpuFreq(CPU_FRQ currFrq)
     {
-        const FrequencyStep frequencySteps[] = {
-            FREQUENCY_LVL_1, 
-            FREQUENCY_LVL_2,
-            FREQUENCY_LVL_3
+        CPU_FRQ freqs[] = { 
+            FRQ_102000KHz,
+            FRQ_204000KHz,
+            FRQ_307200KHz,
+            FRQ_403200KHz,
+            FRQ_518400KHz,
+            FRQ_614400KHz,
+            FRQ_710400KHz,
+            FRQ_825600KHz, 
+            FRQ_921600KHz, 
+            FRQ_1036800KHz, 
+            FRQ_1132800KHz, 
+            FRQ_1224000KHz, 
+            FRQ_1326000KHz, 
+            FRQ_1428000KHz, 
+            FRQ_1479000KHz
         };
 
-        unsigned int i = 0;
-        while(frequencySteps[i] != freq && i < N_FREQUENCY_STEPS)
+        unsigned i = 0;
+        while(i < CPU_FRQ::N_CPU_FREQS-1 && currFrq >= freqs[i])
             i++;
 
-        return frequencySteps[(i+1)%N_FREQUENCY_STEPS];
-    }
+        if (i < CPU_FRQ::N_CPU_FREQS) 
+            return freqs[i];
+        else 
+            return freqs[CPU_FRQ::N_CPU_FREQS-1];
+    };
 
-    void SetFrequency(unsigned int cpu, FrequencyStep freq)
+    CPU_FRQ getNextCpuFreq(unsigned int currFrq)
     {
-        std::string cmd1("sudo cpufreq-set -g userspace -c " + std::to_string(cpu));
-        std::string cmd2("sudo cpufreq-set -c " + std::to_string(cpu) + " -f " + std::to_string(freq));
+        CPU_FRQ freqs[] = { 
+            FRQ_102000KHz,
+            FRQ_204000KHz,
+            FRQ_307200KHz,
+            FRQ_403200KHz,
+            FRQ_518400KHz,
+            FRQ_614400KHz,
+            FRQ_710400KHz,
+            FRQ_825600KHz, 
+            FRQ_921600KHz, 
+            FRQ_1036800KHz, 
+            FRQ_1132800KHz, 
+            FRQ_1224000KHz, 
+            FRQ_1326000KHz, 
+            FRQ_1428000KHz, 
+            FRQ_1479000KHz
+        };
 
-        system(cmd1.c_str());
-        system(cmd2.c_str());
+        unsigned i = 0;
+        while(i < CPU_FRQ::N_CPU_FREQS-1 && currFrq >= static_cast<unsigned int>(freqs[i]))
+            i++;
+
+        
+        if (i < CPU_FRQ::N_CPU_FREQS) 
+            return freqs[i];
+        else 
+            return freqs[CPU_FRQ::N_CPU_FREQS-1];
+    };
+
+    int SetCpuFreq(CPU_FRQ freq){
+        std::stringstream command;
+        command
+            << "sudo echo \"userspace\" | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor && echo "
+            << freq << " | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed && echo "
+            << freq << " | sudo tee /sys/devices/system/cpu/cpu1/cpufreq/scaling_setspeed && echo "
+            << freq << " | sudo tee /sys/devices/system/cpu/cpu2/cpufreq/scaling_setspeed && echo "
+            << freq << " | sudo tee /sys/devices/system/cpu/cpu3/cpufreq/scaling_setspeed" 
+            << std::endl;
+        return std::system(command.str().c_str());
+    };
+
+    GPU_FRQ getCurrGpuFreq()
+    {
+        std::string cmd("cat /sys/devices/57000000.gpu/devfreq/57000000.gpu/cur_freq");
+
+        std::array<char, 128> buffer;
+        std::string result;
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+        if (!pipe) {
+            std::cerr << "ERROR: While calling popen()" << std::endl;
+        }
+        while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            result += buffer.data();
+        }
+
+        return GPU_FRQ(std::stoul(result.c_str()));
+    };
+
+    GPU_FRQ getNextGpuFreq(GPU_FRQ currFrq)
+    {
+        GPU_FRQ freqs[] = { 
+            FRQ_76800000Hz,
+            FRQ_153600000Hz,
+            FRQ_230400000Hz,
+            FRQ_307200000Hz,
+            FRQ_384000000Hz,
+            FRQ_460800000Hz,
+            FRQ_537600000Hz,
+            FRQ_614400000Hz,
+            FRQ_691200000Hz,
+            FRQ_768000000Hz,
+            FRQ_844800000Hz,
+            FRQ_921600000Hz
+        };
+
+        unsigned i = 0;
+        while(i < GPU_FRQ::N_GPU_FREQS-1 && currFrq >= freqs[i])
+            i++;
+
+        if (i < GPU_FRQ::N_GPU_FREQS) 
+            return freqs[i];
+        else 
+            return freqs[GPU_FRQ::N_GPU_FREQS-1];
+    };
+
+    GPU_FRQ getNextGpuFreq(unsigned int currFrq)
+    {
+        GPU_FRQ freqs[] = { 
+            FRQ_76800000Hz,
+            FRQ_153600000Hz,
+            FRQ_230400000Hz,
+            FRQ_307200000Hz,
+            FRQ_384000000Hz,
+            FRQ_460800000Hz,
+            FRQ_537600000Hz,
+            FRQ_614400000Hz,
+            FRQ_691200000Hz,
+            FRQ_768000000Hz,
+            FRQ_844800000Hz,
+            FRQ_921600000Hz
+        };
+
+        unsigned i = 0;
+        while(i < GPU_FRQ::N_GPU_FREQS-1 && currFrq >= static_cast<unsigned int>(freqs[i]))
+            i++;
+
+        
+        if (i < GPU_FRQ::N_GPU_FREQS) 
+            return freqs[i];
+        else 
+            return freqs[GPU_FRQ::N_GPU_FREQS-1];
+    };
+
+    int SetGpuFreq(GPU_FRQ freq){
+        std::stringstream command;
+        command
+            << "sudo echo \"userspace\" | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/governor && echo "
+            << getMinGpuFreq() << " | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/min_freq && echo "
+            << freq  << " | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/max_freq && echo "
+            << freq << " | sudo tee /sys/devices/gpu.0/devfreq/57000000.gpu/min_freq" 
+            << std::endl;
+        return std::system(command.str().c_str());
     };
 }
