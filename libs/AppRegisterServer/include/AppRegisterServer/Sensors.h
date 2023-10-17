2d16307b044b32f88a104373f49476aba99f27d0
Guido Rolle <rolleguido@gmail.com>
2023-10-17 10:34:06 +0200

    Add sampleSensors and getAvg

--- libs/AppRegisterServer/include/AppRegisterServer/Sensors.h
+++ libs/AppRegisterServer/include/AppRegisterServer/Sensors.h
@@ -2,6 +2,7 @@
 #define APP_REGISTER_SERVER_SENSORS
 
 #include <string>
+#include <vector>
 
 #define SENSOR_IN_W  "/sys/bus/i2c/drivers/ina3221x/6-0040/iio:device0/in_power0_input"
 #define SENSOR_IN_A  "/sys/bus/i2c/drivers/ina3221x/6-0040/iio:device0/in_current0_input"
@@ -19,7 +20,6 @@ namespace Sensors
     {
         public:
             void readSensors();
-
             float getSocW(){ return currSocW;};
             float getSocA(){ return currSocA;};
             float getSocV(){ return currSocV;};
@@ -29,9 +29,32 @@ namespace Sensors
             float getGpuW(){ return currGpuW;};
             float getGpuA(){ return currGpuA;};
             float getGpuV(){ return currGpuV;};
+
+            void sampleSensors();
+            float getAvgSocW(){ float avgSocW = getAvg(prevSocW); prevSocW.clear(); return avgSocW; };
+            float getAvgSocA(){ float avgSocA = getAvg(prevSocA); prevSocA.clear(); return avgSocA; };
+            float getAvgSocV(){ float avgSocV = getAvg(prevSocV); prevSocV.clear(); return avgSocV; };
+            float getAvgCpuW(){ float avgCpuW = getAvg(prevCpuW); prevCpuW.clear(); return avgCpuW; };
+            float getAvgCpuA(){ float avgCpuA = getAvg(prevCpuA); prevCpuA.clear(); return avgCpuA; };
+            float getAvgCpuV(){ float avgCpuV = getAvg(prevCpuV); prevCpuV.clear(); return avgCpuV; };
+            float getAvgGpuW(){ float avgGpuW = getAvg(prevGpuW); prevGpuW.clear(); return avgGpuW; };
+            float getAvgGpuA(){ float avgGpuA = getAvg(prevGpuA); prevGpuA.clear(); return avgGpuA; };
+            float getAvgGpuV(){ float avgGpuV = getAvg(prevGpuV); prevGpuV.clear(); return avgGpuV; };
         private:
             //generic read sensor method
             float readSensor(std::string sensorPath);
+            float getAvg(std::vector<float>& vec)
+            {
+                if (vec.size() == 0)
+                    return 0.0;
+
+                float sum = 0.0;
+                for(float num : vec) {
+                    sum += num;
+                }
+
+                return sum / vec.size();
+            }
             
             float currSocW = 0;
             float currSocA = 0;
@@ -42,6 +65,16 @@ namespace Sensors
             float currGpuW = 0;
             float currGpuA = 0;
             float currGpuV = 0;
+
+            std::vector<float> prevSocW;
+            std::vector<float> prevSocA;
+            std::vector<float> prevSocV;
+            std::vector<float> prevCpuW;
+            std::vector<float> prevCpuA;
+            std::vector<float> prevCpuV;
+            std::vector<float> prevGpuW;
+            std::vector<float> prevGpuA;
+            std::vector<float> prevGpuV;
     };
 }
 
--- libs/AppRegisterServer/src/Sensors.cc
+++ libs/AppRegisterServer/src/Sensors.cc
@@ -30,4 +30,16 @@ namespace Sensors
         }
         return value;
     };
+
+    void Sensors::sampleSensors(){
+        prevSocW.push_back(readSensor(SENSOR_IN_W));
+        prevSocA.push_back(readSensor(SENSOR_IN_A));
+        prevSocV.push_back(readSensor(SENSOR_IN_V));
+        prevCpuW.push_back(readSensor(SENSOR_CPU_W));
+        prevCpuA.push_back(readSensor(SENSOR_CPU_A));
+        prevCpuV.push_back(readSensor(SENSOR_CPU_V));
+        prevGpuW.push_back(readSensor(SENSOR_GPU_W));
+        prevGpuA.push_back(readSensor(SENSOR_GPU_A));
+        prevGpuV.push_back(readSensor(SENSOR_GPU_V));
+    };
 }
